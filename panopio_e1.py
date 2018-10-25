acf5e45c2ec0feb1c255af485778cd8964415fb9
Justine Panopio <20anime15@gmail.com>
2018-10-25 22:00:11 +0800

    My Activity

---
+++ panopio_e1.py
@@ -0,0 +1,11 @@
+name = "Ralpg Justine N. Panopio"
+spirit_animal = "Scorpio"
+reason = "I like Music ..."
+hobby = "Play Guitar"
+profession = "Doctor"
+
+print("Iam " + name)
+print("My spirit animal is " + spirit_animal)
+print("because " + reason)
+print("When im not in school, I love to " + hobby)
+print("I dream of being a/an " + profession)
